4fd4ddd719052b9e9244425eb73b150ed222ecb0
Ricardo Torres <rtdeacha@gmail.com>
2020-08-27 23:31:56 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,31 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+    
+
+class over_under(TransformerMixin, BaseEstimator):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X):
+        return self
+    
+    def transform(self, X):
+        return self
+    
+    def fit_transform(self, X):
+        
+        features = X.iloc[:,:-1]
+        target = X.iloc[:,-1]
+        # Primero hacemos over sobre la variable minoritaria
+        over = SMOTE()
+        features, target = over.fit_resample(features,target)
+        
+        # Luego hacemos under sobre la variable mayoritaria
+        under = RandomUnderSampler()
+        features, target = under.fit_resample(features,target)
+        
+        X = pd.concat([pd.DataFrame(features),pd.DataFrame(target).rename(columns ={0:'OBJETIVO'})], axis=1).rename(columns = self.columns, inplace = False)
+        
+        return X 
